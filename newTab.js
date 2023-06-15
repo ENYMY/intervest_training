51af3b7fb48f26e063865957a408fadfc3626836
Shaja Nasir <Shajanasir@gmail.com>
2023-06-15 19:38:47 +0530

    add a single quote with cards

--- newTab.js
+++ newTab.js
@@ -1,6 +1,23 @@
-async function fetchZenQuote(){
-    const url = "https://zenquotes.io/api/random";
-    try{
-        const resonse = await 
-    }
+async function fetchZenQuote() {
+  const url = "https://zenquotes.io/api/random";
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+    const data = await response.json();
+    const quote = data[0].q;
+    console.log(data);
+    const author = data[0].a;
+    const affirmationElement = document.getElementById("dailyAffirmation");
+    affirmationElement.textContent = quote;
+
+    const authorElement = document.getElementById("author");
+    authorElement.textContent = author;
+  } catch (error) {
+    console.log("Error fetching Zen quote:", error);
+  }
 }
+
+fetchZenQuote();
